ff82be4f8f8738a523fad560de5e239bea34e91f
sirdir <phddir@gmail.com>
2018-12-20 23:15:20 +0200

    add allure dependency allure raw lags some rest assured setup

--- src/test/java/io/swagger/petstore/BaseTest.java
+++ src/test/java/io/swagger/petstore/BaseTest.java
@@ -1,4 +1,15 @@
 package io.swagger.petstore;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeSuite;
+
 public class BaseTest {
+
+    @BeforeSuite
+    public void setUp() {
+        RestAssured.baseURI = "https://petstore.swagger.io/"; //todo move to config
+        RestAssured.basePath = "v2/pet"; //todo move to config
+        RestAssured.filters(new AllureRestAssured());
+    }
 }
---
+++ src/test/java/io/swagger/petstore/SmokeTest.java
@@ -0,0 +1,26 @@
+package io.swagger.petstore;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest extends BaseTest {
+
+    @Test
+    public void postTest(){
+
+    }
+
+    @Test
+    public void getTest(){
+
+    }
+
+    @Test
+    public void updateTest(){
+
+    }
+
+    @Test
+    public void deleteTest(){
+
+    }
+}
